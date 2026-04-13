032a4aa772608f5762a50daa30084a42dd6d04f0
harryli0088 <harryli0088@gmail.com>
2026-04-13 14:38:17 -0400

    brought data.ts back

---
+++ scripts/data.ts
@@ -0,0 +1,13 @@
+export const data = {
+  chinese: {
+    title: "春望",
+    author: "杜甫",
+    content: `...`,
+  },
+  translation: {
+    title: "Spring View",
+    author: "Du Fu",
+    content: `...`, //each line here should map to the lines in data.chinese.content
+  },
+  type: "poem",
+}
